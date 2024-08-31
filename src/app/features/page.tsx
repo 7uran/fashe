4bca8e23d036bebfab7de407722b3e2e9ba83334
7uran <turan.official21@gmail.com>
2024-09-01 03:13:30 +0400

    hot-fix:header logo

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -42,52 +42,55 @@ const FeaturePage = () => {
                 <h1 className='uppercase absolute text-5xl z-[1] text-white font-bold'>cart</h1>
             </div>
             <div className='max-w-[1176px] mx-auto py-20'>
-                <table className="min-w-full bg-white">
-                    <thead>
-                        <tr>
-                            <th className="py-2 px-4 border-b border-gray-300"></th>
-                            <th className="py-2 px-4 border-b border-gray-300">Product</th>
-                            <th className="py-2 px-4 border-b border-gray-300">Price</th>
-                            <th className="py-2 px-4 border-b border-gray-300">Quantity</th>
-                            <th className="py-2 px-4 border-b border-gray-300">Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product, index) => (
-                            <tr className='border' key={index}>
-                                <td className="p-10 border-b border-gray-300">
-                                    <div onClick={() => deleteProduct(index)} className='delete bg-black group w-fit relative flex items-center justify-center text-white text-xl'>
-                                        <img src={product.img} alt={product.title} className="duration-500 group-hover:opacity-35 transition cursor-pointer h-[120px] object-cover" />
-                                        <IoCloseSharp
-                                            className='absolute opacity-0 group-hover:opacity-100 duration-500 cursor-pointer'
-                                            
-                                        />
-                                    </div>
-                                </td>
-                                <td className="p-10 border-b border-gray-300">{product.title}</td>
-                                <td className="py-2 border-b border-gray-300">${product.price.toFixed(2)}</td>
-                                <td className="py-2 border-b border-gray-300">
-                                    <div className='flex items-center justify-center border rounded-sm w-fit'>
-                                        <button
-                                            className="bg-gray-200 w-[48px] h-[48px] rounded-sm"
-                                            onClick={() => decreaseQuantity(index)}
-                                        >
-                                            -
-                                        </button>
-                                        <span className="px-4">{product.quantity}</span>
-                                        <button
-                                            className="bg-gray-200 w-[48px] h-[48px] rounded-sm"
-                                            onClick={() => increaseQuantity(index)}
-                                        >
-                                            +
-                                        </button>
-                                    </div>
-                                </td>
-                                <td className="py-2 border-b border-gray-300">${(product.price * product.quantity).toFixed(2)}</td>
+                {products.length === 0 ? (
+                    <p className='text-center text-xl font-semibold'>No items here</p>
+                ) : (
+                    <table className="min-w-full bg-white">
+                        <thead>
+                            <tr>
+                                <th className="py-2 px-4 border-b border-gray-300"></th>
+                                <th className="py-2 px-4 border-b border-gray-300">Product</th>
+                                <th className="py-2 px-4 border-b border-gray-300">Price</th>
+                                <th className="py-2 px-4 border-b border-gray-300">Quantity</th>
+                                <th className="py-2 px-4 border-b border-gray-300">Total</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {products.map((product, index) => (
+                                <tr className='border' key={index}>
+                                    <td className="p-10 border-b border-gray-300">
+                                        <div onClick={() => deleteProduct(index)} className='delete bg-black group w-fit relative flex items-center justify-center text-white text-xl'>
+                                            <img src={product.img} alt={product.title} className="duration-500 group-hover:opacity-35 transition cursor-pointer h-[120px] object-cover" />
+                                            <IoCloseSharp
+                                                className='absolute opacity-0 group-hover:opacity-100 duration-500 cursor-pointer'
+                                            />
+                                        </div>
+                                    </td>
+                                    <td className="p-10 border-b border-gray-300">{product.title}</td>
+                                    <td className="py-2 border-b border-gray-300">${product.price.toFixed(2)}</td>
+                                    <td className="py-2 border-b border-gray-300">
+                                        <div className='flex items-center justify-center border rounded-sm w-fit'>
+                                            <button
+                                                className="bg-gray-200 w-[48px] h-[48px] rounded-sm"
+                                                onClick={() => decreaseQuantity(index)}
+                                            >
+                                                -
+                                            </button>
+                                            <span className="px-4">{product.quantity}</span>
+                                            <button
+                                                className="bg-gray-200 w-[48px] h-[48px] rounded-sm"
+                                                onClick={() => increaseQuantity(index)}
+                                            >
+                                                +
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td className="py-2 border-b border-gray-300">${(product.price * product.quantity).toFixed(2)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
         </div>
     );
